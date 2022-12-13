f83baeac02ce02d5bb5f417293115cfa73f64e6a
JanSzewczyk <jan.szewczyk1997@gmail.com>
2022-12-13 21:42:05 +0100

    refactor: refactor preset file

--- tailwindcss/main-preset.js
+++ tailwindcss/main-preset.js
@@ -2,75 +2,75 @@
 module.exports = {
   darkMode: "class",
   theme: {
+    colors: {
+      black: "rgb(var(--black) / <alpha-value>)",
+      error: {
+        100: "rgb(var(--error-100) / <alpha-value>)",
+        200: "rgb(var(--error-200) / <alpha-value>)",
+        300: "rgb(var(--error-300) / <alpha-value>)",
+        400: "rgb(var(--error-400) / <alpha-value>)",
+        500: "rgb(var(--error-500) / <alpha-value>)",
+        600: "rgb(var(--error-600) / <alpha-value>)",
+        700: "rgb(var(--error-700) / <alpha-value>)",
+        800: "rgb(var(--error-800) / <alpha-value>)",
+        900: "rgb(var(--error-900) / <alpha-value>)"
+      },
+      gray: {
+        100: "rgb(var(--gray-100) / <alpha-value>)",
+        200: "rgb(var(--gray-200) / <alpha-value>)",
+        300: "rgb(var(--gray-300) / <alpha-value>)",
+        400: "rgb(var(--gray-400) / <alpha-value>)",
+        500: "rgb(var(--gray-500) / <alpha-value>)",
+        600: "rgb(var(--gray-600) / <alpha-value>)",
+        700: "rgb(var(--gray-700) / <alpha-value>)",
+        800: "rgb(var(--gray-800) / <alpha-value>)",
+        900: "rgb(var(--gray-900) / <alpha-value>)"
+      },
+      primary: {
+        100: "rgb(var(--primary-100) / <alpha-value>)",
+        200: "rgb(var(--primary-200) / <alpha-value>)",
+        300: "rgb(var(--primary-300) / <alpha-value>)",
+        400: "rgb(var(--primary-400) / <alpha-value>)",
+        500: "rgb(var(--primary-500) / <alpha-value>)",
+        600: "rgb(var(--primary-600) / <alpha-value>)",
+        700: "rgb(var(--primary-700) / <alpha-value>)",
+        800: "rgb(var(--primary-800) / <alpha-value>)",
+        900: "rgb(var(--primary-900) / <alpha-value>)"
+      },
+      success: {
+        100: "rgb(var(--success-100) / <alpha-value>)",
+        200: "rgb(var(--success-200) / <alpha-value>)",
+        300: "rgb(var(--success-300) / <alpha-value>)",
+        400: "rgb(var(--success-400) / <alpha-value>)",
+        500: "rgb(var(--success-500) / <alpha-value>)",
+        600: "rgb(var(--success-600) / <alpha-value>)",
+        700: "rgb(var(--success-700) / <alpha-value>)",
+        800: "rgb(var(--success-800) / <alpha-value>)",
+        900: "rgb(var(--success-900) / <alpha-value>)"
+      },
+      transparent: "transparent",
+      typography: {
+        DEFAULT: "rgb(var(--text-color) / <alpha-value>)",
+        primary: "rgb(var(--text-color) / 1)",
+        secondary: "rgb(var(--text-color) / 0.60)",
+        disabled: "rgb(var(--text-color) / 0.43)"
+      },
+      warning: {
+        100: "rgb(var(--warning-100) / <alpha-value>)",
+        200: "rgb(var(--warning-200) / <alpha-value>)",
+        300: "rgb(var(--warning-300) / <alpha-value>)",
+        400: "rgb(var(--warning-400) / <alpha-value>)",
+        500: "rgb(var(--warning-500) / <alpha-value>)",
+        600: "rgb(var(--warning-600) / <alpha-value>)",
+        700: "rgb(var(--warning-700) / <alpha-value>)",
+        800: "rgb(var(--warning-800) / <alpha-value>)",
+        900: "rgb(var(--warning-900) / <alpha-value>)"
+      },
+      white: "rgb(var(--white) / <alpha-value>)"
+    },
     extend: {
       borderRadius: {
         "1/2": "50%"
-      },
-      colors: {
-        black: "rgb(var(--black) / <alpha-value>)",
-        error: {
-          100: "rgb(var(--error-100) / <alpha-value>)",
-          200: "rgb(var(--error-200) / <alpha-value>)",
-          300: "rgb(var(--error-300) / <alpha-value>)",
-          400: "rgb(var(--error-400) / <alpha-value>)",
-          500: "rgb(var(--error-500) / <alpha-value>)",
-          600: "rgb(var(--error-600) / <alpha-value>)",
-          700: "rgb(var(--error-700) / <alpha-value>)",
-          800: "rgb(var(--error-800) / <alpha-value>)",
-          900: "rgb(var(--error-900) / <alpha-value>)"
-        },
-        gray: {
-          100: "rgb(var(--gray-100) / <alpha-value>)",
-          200: "rgb(var(--gray-200) / <alpha-value>)",
-          300: "rgb(var(--gray-300) / <alpha-value>)",
-          400: "rgb(var(--gray-400) / <alpha-value>)",
-          500: "rgb(var(--gray-500) / <alpha-value>)",
-          600: "rgb(var(--gray-600) / <alpha-value>)",
-          700: "rgb(var(--gray-700) / <alpha-value>)",
-          800: "rgb(var(--gray-800) / <alpha-value>)",
-          900: "rgb(var(--gray-900) / <alpha-value>)"
-        },
-        primary: {
-          100: "rgb(var(--primary-100) / <alpha-value>)",
-          200: "rgb(var(--primary-200) / <alpha-value>)",
-          300: "rgb(var(--primary-300) / <alpha-value>)",
-          400: "rgb(var(--primary-400) / <alpha-value>)",
-          500: "rgb(var(--primary-500) / <alpha-value>)",
-          600: "rgb(var(--primary-600) / <alpha-value>)",
-          700: "rgb(var(--primary-700) / <alpha-value>)",
-          800: "rgb(var(--primary-800) / <alpha-value>)",
-          900: "rgb(var(--primary-900) / <alpha-value>)"
-        },
-        success: {
-          100: "rgb(var(--success-100) / <alpha-value>)",
-          200: "rgb(var(--success-200) / <alpha-value>)",
-          300: "rgb(var(--success-300) / <alpha-value>)",
-          400: "rgb(var(--success-400) / <alpha-value>)",
-          500: "rgb(var(--success-500) / <alpha-value>)",
-          600: "rgb(var(--success-600) / <alpha-value>)",
-          700: "rgb(var(--success-700) / <alpha-value>)",
-          800: "rgb(var(--success-800) / <alpha-value>)",
-          900: "rgb(var(--success-900) / <alpha-value>)"
-        },
-        transparent: "transparent",
-        typography: {
-          DEFAULT: "rgb(var(--text-color) / <alpha-value>)",
-          primary: "rgb(var(--text-color) / 1)",
-          secondary: "rgb(var(--text-color) / 0.60)",
-          disabled: "rgb(var(--text-color) / 0.43)"
-        },
-        warning: {
-          100: "rgb(var(--warning-100) / <alpha-value>)",
-          200: "rgb(var(--warning-200) / <alpha-value>)",
-          300: "rgb(var(--warning-300) / <alpha-value>)",
-          400: "rgb(var(--warning-400) / <alpha-value>)",
-          500: "rgb(var(--warning-500) / <alpha-value>)",
-          600: "rgb(var(--warning-600) / <alpha-value>)",
-          700: "rgb(var(--warning-700) / <alpha-value>)",
-          800: "rgb(var(--warning-800) / <alpha-value>)",
-          900: "rgb(var(--warning-900) / <alpha-value>)"
-        },
-        white: "rgb(var(--white) / <alpha-value>)"
       },
       fontFamily: {
         sans: ["'Open Sans', sans-serif"],
