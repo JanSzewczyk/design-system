f3c77fdd1fdf1c85c8e281011f82ab676d51bb00
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-02-17 23:04:57 +0100

    fix(build): fix built structure

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,76 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.tsx"],
-  dts: true,
-  treeshake: true,
-  minify: true,
-  format: ["esm", "cjs"],
-  external: ["react", "react-dom"]
-});
+const dirNames = ["components", "contexts", "theme"];
+
+export default defineConfig([
+  {
+    entry: ["./src/components/!(index.ts?(x))/index.ts?(x)"],
+    dts: true,
+    treeshake: true,
+    minify: true,
+    format: ["esm", "cjs"],
+    external: ["react", "react-dom"],
+    outDir: "components"
+  },
+  {
+    entry: ["./src/components/index.ts?(x)"],
+    dts: true,
+    treeshake: true,
+    minify: true,
+    format: ["esm", "cjs"],
+    external: ["react", "react-dom"],
+    outDir: "components",
+    bundle: false
+  },
+
+  {
+    entry: ["./src/contexts/theme/index.ts?(x)"],
+    dts: true,
+    treeshake: true,
+    minify: true,
+    format: ["esm", "cjs"],
+    external: ["react", "react-dom"],
+    outDir: "contexts/theme"
+  },
+  {
+    entry: ["./src/contexts/index.ts?(x)"],
+    dts: true,
+    treeshake: true,
+    minify: true,
+    format: ["esm", "cjs"],
+    external: ["react", "react-dom"],
+    outDir: "contexts",
+    bundle: false
+  },
+
+  {
+    entry: ["./src/hooks/useTheme/index.ts?(x)"],
+    dts: true,
+    treeshake: true,
+    minify: true,
+    format: ["esm", "cjs"],
+    external: ["react", "react-dom"],
+    outDir: "hooks/useTheme"
+  },
+  {
+    entry: ["./src/hooks/index.ts?(x)"],
+    dts: true,
+    treeshake: true,
+    minify: true,
+    format: ["esm", "cjs"],
+    external: ["react", "react-dom"],
+    outDir: "hooks",
+    bundle: false
+  },
+
+  {
+    entry: ["./src/index.ts?(x)"],
+    dts: true,
+    treeshake: true,
+    minify: true,
+    format: ["esm", "cjs"],
+    external: ["react", "react-dom"],
+    outDir: ".",
+    bundle: false
+  }
+]);
