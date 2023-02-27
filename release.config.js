b55df223db2ac3cb0c1c3eb7cf00473a6f069d77
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-02-27 12:00:00 +0100

    ci(release): start using `@szum-tech/semantic-release-preset` package

--- release.config.js
+++ release.config.js
@@ -1,12 +1,5 @@
 /** @type {import('semantic-release').Options} */
 module.exports = {
   branches: ["main"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    ["@semantic-release/npm"],
-    ["@semantic-release/git", { assets: ["CHANGELOG.md", "package.json"] }],
-    "@semantic-release/github"
-  ]
+  extends: "@szum-tech/semantic-release-preset"
 };
