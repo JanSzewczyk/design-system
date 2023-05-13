b7fa5600ed89c0471e77081c55b12090349f6b81
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-05-13 18:58:10 +0200

    feat(tooltip): create `Tooltip` component

---
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+import { Meta, StoryObj } from "@storybook/react";
+
+import Tooltip from "./Tooltip.component";
+import { TooltipProvider } from "./Tooltip.provider";
+
+import Button from "../Button";
+
+const meta = {
+  title: "Components/Tooltip",
+  component: Tooltip,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <TooltipProvider>
+        <Story />
+      </TooltipProvider>
+    )
+  ]
+} satisfies Meta<typeof Tooltip>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <Tooltip {...args}>
+      <Button>Click</Button>
+    </Tooltip>
+  ),
+  args: {
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet erat turpis, ut lacinia lacus tincidunt sed." +
+      " In non lorem est. Nunc auctor iaculis rhoncus. Vivamus blandit tempus tellus sed luctus. In porta arcu erat, at " +
+      "laoreet ante scelerisque sed. Fusce egestas felis non nulla porttitor, eu mollis nunc porta. Maecenas maximus sem " +
+      "at malesuada convallis.",
+    side: "bottom"
+  }
+};
--- src/theme/main-preset.js
+++ src/theme/main-preset.js
@@ -113,6 +113,30 @@ module.exports = {
       width: {
         128: "32.0rem",
         300: "75.0rem"
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: "translateY(-2px)" },
+          to: { opacity: 1, transform: "translateY(0)" }
+        },
+        slideLeftAndFade: {
+          from: { opacity: 0, transform: "translateX(2px)" },
+          to: { opacity: 1, transform: "translateX(0)" }
+        },
+        slideUpAndFade: {
+          from: { opacity: 0, transform: "translateY(2px)" },
+          to: { opacity: 1, transform: "translateY(0)" }
+        },
+        slideRightAndFade: {
+          from: { opacity: 0, transform: "translateX(-2px)" },
+          to: { opacity: 1, transform: "translateX(0)" }
+        }
+      },
+      animation: {
+        slideDownAndFade: "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideLeftAndFade: "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideRightAndFade: "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)"
       }
     }
   },
