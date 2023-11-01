3f05c01111cbc56a3d7da460088f64770b2b188d
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-11-01 17:56:25 +0100

    fix(styles): fix `container` class styles

--- src/theme/main-preset.js
+++ src/theme/main-preset.js
@@ -4,7 +4,10 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem"
+      },
       screens: {
         "2xl": "1400px"
       }
