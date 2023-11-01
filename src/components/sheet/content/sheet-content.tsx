1fabc191e2448a87902ee18ab66e47db849c35dd
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-11-01 15:56:42 +0100

    refactor(colors): change colors names

--- src/components/sheet/content/sheet-content.tsx
+++ src/components/sheet/content/sheet-content.tsx
@@ -27,7 +27,7 @@ export const SheetContent = React.forwardRef<
         {...props}
       >
         {children}
-        <ReactSheet.Close className="focus:ring-ring absolute right-4 top-4 rounded-sm opacity-70 ring-primary-500 ring-offset-app-primary transition-opacity data-[state=open]:bg-app-primary hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+        <ReactSheet.Close className="focus:ring-ring absolute right-4 top-4 rounded-sm opacity-70 ring-primary-500 ring-offset-foreground transition-opacity data-[state=open]:bg-foreground hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
           <Cross1Icon className="h-4 w-4" />
           <span className="sr-only">Close</span>
         </ReactSheet.Close>
