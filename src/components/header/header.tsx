1ee91219ddf4f50e3090cf0e92e3bfd93402a8c6
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-11-01 16:48:31 +0100

    fix(header): update `Header` height

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,7 +7,7 @@ export type HeaderProps = {
 export function Header({ children }: HeaderProps) {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-gray-400 bg-foreground/95 backdrop-blur supports-[backdrop-filter]:bg-foreground/60">
-      <div className="container flex h-14 items-center">{children}</div>
+      <div className="container flex h-16 items-center">{children}</div>
     </header>
   );
 }
