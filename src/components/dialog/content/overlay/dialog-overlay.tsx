552b35e911b9319bbb67416e34422873eec075bd
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-11-19 21:01:39 +0100

    fix(z-indexes): fix z-indexes

--- src/components/dialog/content/overlay/dialog-overlay.tsx
+++ src/components/dialog/content/overlay/dialog-overlay.tsx
@@ -11,7 +11,7 @@ export const DialogOverlay = React.forwardRef<
   return (
     <ReactDialog.Overlay
       {...props}
-      className="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+      className="fixed inset-0 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
       ref={ref}
     />
   );
