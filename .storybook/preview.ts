7f731b8d2dcb45fe09b246515215af77a7409001
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-01-25 19:12:21 +0100

    feat(storybook): upgrade storybook to latest version

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,35 +1,36 @@
 import darkTheme from "./theme/dark";
 import lightTheme from "./theme/light";
-
+import { type Preview } from "@storybook/react";
 import "../src/theme/global.css";
 
 import { DocsContainer } from "./components/DocsContainer";
 
-export const parameters = {
-  darkMode: {
-    current: "light",
-    classTarget: "html",
-    stylePreview: true,
-    // Override the default dark theme
-    dark: darkTheme,
-    // Override the default light theme
-    light: lightTheme
-  },
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  backgrounds: { disable: true },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/
+export default {
+  parameters: {
+    darkMode: {
+      current: "light",
+      classTarget: "html",
+      stylePreview: true,
+      // Override the default dark theme
+      dark: darkTheme,
+      // Override the default light theme
+      light: lightTheme
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: { disable: true },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/
+      }
+    },
+    container: DocsContainer,
+    docs: {
+      controls: {
+        sort: "requiredFirst"
+      }
     }
   },
-  docs: {
-    controls: {
-      sort: "requiredFirst"
-    },
-    container: DocsContainer
-  },
-  layout: "centered"
-};
-
-export const decorators = [];
+  decorators: [],
+  tags: ["autodocs"]
+} satisfies Preview;
