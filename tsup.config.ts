04561ee57823666959420672993d54b950310d25
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-02-07 21:47:31 +0100

    build(tsuo): update `tsup` configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,40 +1,28 @@
 import { defineConfig } from "tsup";
 
-const env = process.env.NODE_ENV || "development";
-
-// TODO refactor this
-
 export default defineConfig([
   {
     splitting: true,
-    clean: env !== "production",
+    clean: true,
     dts: true,
     format: ["esm", "cjs"],
-    minify: env === "production",
+    minify: false,
     bundle: true,
     treeshake: true,
-    skipNodeModulesBundle: false,
     outDir: "dist",
     entry: ["./src/index.ts?(x)"],
-    external: ["react", "react-dom", "class-variance-authority", "@radix-ui/*", "react-hook-form", "tailwind-merge"]
+    external: ["react", "react-dom", "class-variance-authority", "@radix-ui", "react-hook-form", "tailwind-merge"]
   },
   {
     splitting: true,
-    clean: env !== "production",
+    clean: true,
     dts: true,
     format: ["esm", "cjs"],
-    minify: env === "production",
+    minify: true,
     bundle: true,
     treeshake: true,
     skipNodeModulesBundle: false,
     outDir: "icons",
     entry: ["./src/icons/index.tsx"]
-  },
-  {
-    entry: ["./src/theme/main-preset.ts"],
-    minify: false,
-    bundle: true,
-    dts: true,
-    outDir: env === "production" ? "theme" : "dist/theme"
   }
 ]);
