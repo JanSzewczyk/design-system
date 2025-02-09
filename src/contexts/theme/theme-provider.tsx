e9a4344bda045c16040180e542837ce91e46c77a
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-02-09 15:06:11 +0100

    refactor(components): remove theme provider from package

--- src/contexts/theme/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-export type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
