6702b619134a9b5981a681679a08b92f962d4f6e
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-03-02 22:23:46 +0100

    fix(deps): bump deps

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -4,8 +4,6 @@ import { expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import { TrashIcon } from "../../icons";
-
 import { Button } from "./Button";
 
 const mockedFn = vi.fn();
@@ -76,27 +74,5 @@ describe("Component > Button", () => {
     expect(button).toHaveAttribute("aria-disabled");
     expect(button).toHaveAttribute("href", "");
     expect(button).toHaveTextContent(/link/i);
-  });
-
-  test("replace start icon with loading icon", async () => {
-    render(
-      <Button startIcon={<TrashIcon />} loading>
-        Label
-      </Button>
-    );
-
-    const button = screen.getByRole("button");
-    expect(button).toHaveAttribute("type", "button");
-    expect(button).toHaveAttribute("aria-disabled");
-    expect(button).toBeDisabled();
-    expect(button).toHaveAttribute("data-state", "loading");
-
-    const loadingContainer = screen.getByRole("progressbar");
-    expect(loadingContainer).toBeInTheDocument();
-    expect(button).toContainElement(loadingContainer);
-
-    const loadingIcon = screen.getByLabelText("Loading");
-    expect(loadingIcon).toBeInTheDocument();
-    expect(loadingContainer).toContainElement(loadingIcon);
   });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
 import tsConfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 import { defineConfig } from "vitest/config";
 
 const reporters = process.env.CI ? ["dot", "github-actions"] : ["dot"];
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
+  plugins: [tsConfigPaths(), react()],
   test: {
     globals: true,
     environment: "happy-dom",
