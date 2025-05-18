0db1eb786409d57e7ce9549914ee668e6da911ee
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-05-18 23:31:31 +0200

    fix: add `use client` directives

---
+++ src/scripts/post-build.js
@@ -0,0 +1,18 @@
+import fs from "fs/promises";
+
+async function updateFilesWithText(filePaths, text) {
+  for (const filePath of filePaths) {
+    try {
+      const content = await fs.readFile(filePath, "utf8");
+      const updatedContent = text + content;
+      await fs.writeFile(filePath, updatedContent, "utf8");
+      console.log(`Updated: ${filePath}`);
+    } catch (err) {
+      console.error(`Error updating ${filePath}:`, err);
+    }
+  }
+}
+
+updateFilesWithText(["dist/components/index.js", "dist/components/index.cjs"], '"use client";\n\n').then(() => {
+  console.log("Files updated successfully");
+});
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,39 +1,27 @@
 import { defineConfig } from "tsup";
 
-export function useClientPlugin() {
-  return {
-    name: "use-client-plugin",
-    renderChunk(code: string) {
-      return { code: `"use client";\n${code}` };
-    }
-  };
-}
-
-export default defineConfig([
-  {
-    splitting: true,
-    clean: true,
-    dts: true,
-    format: ["esm", "cjs"],
-    bundle: true,
-    treeshake: true,
-    outDir: "dist",
-    entry: ["src/components/*/index.tsx", "src/components/index.tsx", "src/utils/index.ts", "src/icons/index.tsx"],
-    esbuildOptions(options) {
-      // the directory structure will be the same as the source
-      options.outbase = "./src";
-    },
-    external: [
-      "react",
-      "react-dom",
-      "class-variance-authority",
-      "radix-ui",
-      "@radix-ui/react-slot",
-      "react-hook-form",
-      "tailwind-merge",
-      "clx",
-      "@radix-ui/react-icons"
-    ],
-    plugins: [useClientPlugin()]
-  }
-]);
+export default defineConfig({
+  splitting: true,
+  clean: true,
+  dts: true,
+  format: ["esm", "cjs"],
+  bundle: true,
+  treeshake: true,
+  outDir: "dist",
+  entry: ["src/components/*/index.tsx", "src/components/index.tsx", "src/utils/index.ts", "src/icons/index.tsx"],
+  esbuildOptions(options) {
+    // the directory structure will be the same as the source
+    options.outbase = "./src";
+  },
+  external: [
+    "react",
+    "react-dom",
+    "class-variance-authority",
+    "radix-ui",
+    "@radix-ui/react-slot",
+    "react-hook-form",
+    "tailwind-merge",
+    "clx",
+    "@radix-ui/react-icons"
+  ]
+});
