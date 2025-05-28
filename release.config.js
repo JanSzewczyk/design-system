fd45354bd7595f3f7088f4c861a524bbb8ab69f1
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-05-29 00:50:48 +0200

    build(semantic-release): update Semantic Release configuration

--- release.config.js
+++ release.config.js
@@ -1,1 +1,3 @@
-export { default } from "@szum-tech/semantic-release-config/with-npm";
+import { getConfig } from "@szum-tech/semantic-release-config";
+
+export default getConfig({ features: { npmPublish: true, mdx: true } });
