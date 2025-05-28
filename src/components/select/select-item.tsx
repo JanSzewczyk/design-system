e8732a905d28a60091f012e91b45bd74f607a83c
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-05-29 00:44:47 +0200

    refactor(Select): refactor `Select` component

--- src/components/select/select-item.tsx
+++ src/components/select/select-item.tsx
@@ -3,13 +3,17 @@ import * as React from "react";
 import { Select as ReactSelect } from "radix-ui";
 
 import { CheckIcon } from "@radix-ui/react-icons";
+import { cn } from "~/utils";
 
 export type SelectItemProps = React.ComponentProps<typeof ReactSelect.Item>;
 
-export function SelectItem({ children, ...props }: SelectItemProps) {
+export function SelectItem({ children, className, ...props }: SelectItemProps) {
   return (
     <ReactSelect.Item
-      className="text-body-2 data-[state=checked]:bg-primary-300 flex w-full flex-row items-center justify-between px-3 py-2 text-gray-100 select-none data-[disabled]:pointer-events-none data-[highlighted]:bg-gray-400 data-[highlighted]:outline-none"
+      className={cn(
+        "text-body-2 data-[state=checked]:bg-primary-500 data-[state=checked]:text-app-foreground flex w-full flex-row items-center justify-between px-3 py-2 text-gray-100 select-none data-[disabled]:pointer-events-none data-[highlighted]:bg-gray-900 data-[highlighted]:outline-none data-[state=checked]:outline-none",
+        className
+      )}
       {...props}
     >
       <ReactSelect.ItemText className="flex-1">{children}</ReactSelect.ItemText>
