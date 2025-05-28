dd256e8b0b4ce182caaf0acbf1ccfb81e0a4c06d
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-05-29 00:43:55 +0200

    refactor(Label): refactor `Label` component

--- src/components/label/label.tsx
+++ src/components/label/label.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 
 import { Label as ReactLabel } from "radix-ui";
-import { twMerge } from "tailwind-merge";
+
+import { cn } from "~/utils";
 
 export type LabelProps = React.ComponentProps<typeof ReactLabel.Root>;
 
 export function Label({ className, ...props }: LabelProps) {
   return (
     <ReactLabel.Root
-      className={twMerge("text-subtitle-1 peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
+      className={cn("text-subtitle-1 peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
       {...props}
     />
   );
