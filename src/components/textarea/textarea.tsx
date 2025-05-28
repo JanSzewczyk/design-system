cd8e3201d1a73b563c9a919029fc127cea16f366
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-05-29 00:45:54 +0200

    refactor(Textarea): refactor `Textarea` component

--- src/components/textarea/textarea.tsx
+++ src/components/textarea/textarea.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { textareaCva } from "./Textarea.styles";
+import { textareaCva } from "./textarea.styles";
 
 export type TextareaProps = React.DetailedHTMLProps<
   React.TextareaHTMLAttributes<HTMLTextAreaElement>,
