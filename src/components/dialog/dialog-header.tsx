4cce75766478c72e48dee94715ed4067345755f2
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-05-29 00:41:47 +0200

    refactor(Dialog): refactor `Dialog` components

--- src/components/dialog/dialog-header.tsx
+++ src/components/dialog/dialog-header.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 
-import { twMerge } from "tailwind-merge";
+import { cn } from "~/utils";
 
 export type DialogHeaderProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 export function DialogHeader({ className, ...props }: DialogHeaderProps) {
-  return <div className={twMerge("mb-4 flex flex-col space-y-1.5 text-center sm:text-left", className)} {...props} />;
+  return <div className={cn("mb-4 flex flex-col space-y-1.5 text-center sm:text-left", className)} {...props} />;
 }
