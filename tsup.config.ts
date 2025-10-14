99d638e9b14e6f39960cc5de17e9172a3e06db4f
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-10-14 23:11:29 +0200

    chore(build): expand entry points and exports for hooks and contexts
    Include `hooks` and `contexts` in build entry points and package exports to ensure proper bundling and usability.

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,7 +8,14 @@ export default defineConfig({
   bundle: true,
   treeshake: true,
   outDir: "dist",
-  entry: ["src/components/*/index.tsx", "src/components/index.tsx", "src/utils/index.ts", "src/icons/index.tsx"],
+  entry: [
+    "src/components/*/index.tsx",
+    "src/components/index.tsx",
+    "src/utils/index.ts",
+    "src/hooks/index.tsx",
+    "src/contexts/index.tsx",
+    "src/icons/index.tsx"
+  ],
   esbuildOptions(options) {
     // the directory structure will be the same as the source
     options.outbase = "./src";
