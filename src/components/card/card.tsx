c76037d039be4e350351d1b91f81d884ad592fe1
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-10-19 17:16:01 +0200

    feat(separator): add `data-slot` attribute for enhanced slot targeting

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 
-import { twMerge } from "tailwind-merge";
+import { cn } from "~/utils";
 
 export type CardProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 export function Card({ className, ...props }: CardProps) {
-  return <div className={twMerge("bg-app-foreground h-full rounded border border-gray-800", className)} {...props} />;
+  return <div className={cn("bg-app-foreground h-full rounded border border-gray-800", className)} {...props} />;
 }
