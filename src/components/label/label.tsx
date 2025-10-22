e2e72d4caab0054f0c51468e2ccbc6b58508d003
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-10-22 10:45:45 +0200

    feat(label): add `data-slot` attribute and update styles

--- src/components/label/label.tsx
+++ src/components/label/label.tsx
@@ -9,7 +9,11 @@ export type LabelProps = React.ComponentProps<typeof ReactLabel.Root>;
 export function Label({ className, ...props }: LabelProps) {
   return (
     <ReactLabel.Root
-      className={cn("text-subtitle-1 peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
+      data-slot="label"
+      className={cn(
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        className
+      )}
       {...props}
     />
   );
