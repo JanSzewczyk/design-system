5a8326d721c3ffed9ac4d428d4447dd5223d1ffc
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-10-22 11:42:42 +0200

    chore(form): remove `Form` component and related subcomponents
    Delete `Form` and its associated subcomponents, including stories and utility hooks. Drop `react-hook-form` dependency from the project.

--- src/components/form/form.tsx
+++
@@ -1,4 +0,0 @@
-import { FormProvider, type FormProviderProps } from "react-hook-form";
-
-export type FormProps = FormProviderProps;
-export const Form = FormProvider;
--- src/components/form/item/form-item.context.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export type FormItemContextValue = {
-  id: string;
-};
-
-export const FormItemContext = React.createContext<FormItemContextValue>({} as FormItemContextValue);
