be268ab447cb4c0bc66f16897956d264200dfffe
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-11-11 11:07:07 +0100

    chore(dependencies): update import path for storybook-addon-tag-badges and adjust semantic-release version

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,5 @@
-import { defaultConfig, type TagBadgeParameters } from "storybook-addon-tag-badges";
-
 import { addons } from "storybook/manager-api";
+import { defaultConfig, type TagBadgeParameters } from "storybook-addon-tag-badges/manager-helpers";
 
 addons.setConfig({
   tagBadges: [
