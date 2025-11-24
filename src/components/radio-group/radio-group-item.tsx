a030554fe43ffd48136ad56a0da71865a7f13aed
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-11-25 00:01:46 +0100

    fix(layout): reorder class names for consistency in dialog and badge components

--- src/components/radio-group/radio-group-item.tsx
+++ src/components/radio-group/radio-group-item.tsx
@@ -24,7 +24,7 @@ export function RadioGroupItem({ className, ...props }: RadioGroupItemProps) {
         data-slot="radio-group-indicator"
         className="relative flex items-center justify-center"
       >
-        <CircleIcon className="fill-primary absolute left-1/2 top-1/2 size-2 -translate-x-1/2 -translate-y-1/2" />
+        <CircleIcon className="fill-primary absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
