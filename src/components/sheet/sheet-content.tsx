e2496d7b5496dc596caa4faf8c40c115f98c87aa
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-11-25 00:01:46 +0100

    fix(layout): reorder class names for consistency in dialog and badge components

--- src/components/sheet/sheet-content.tsx
+++ src/components/sheet/sheet-content.tsx
@@ -18,7 +18,7 @@ export function SheetContent({ side = "right", className, children, ...props }: 
       <ReactSheet.Overlay className="bg-app-background/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 backdrop-blur-sm" />
       <ReactSheet.Content className={cn(sheetContentStyles({ side }), className)} {...props}>
         {children}
-        <ReactSheet.Close className="focus:ring-ring ring-primary-500 ring-offset-app-foreground data-[state=open]:bg-app-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+        <ReactSheet.Close className="focus:ring-ring ring-primary-500 ring-offset-app-foreground data-[state=open]:bg-app-foreground absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none">
           <X className="size-4" />
           <span className="sr-only">Close</span>
         </ReactSheet.Close>
