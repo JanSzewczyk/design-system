318d632871650f6f4f1b031e2e5ffe36c31ac5e0
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-12-14 22:21:30 +0100

    fix(select): fix `SelectGroup` component

--- src/components/select/select-group.tsx
+++ src/components/select/select-group.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import * as SelectPrimitive from "@radix-ui/react-select";
+import { Select as SelectPrimitive } from "radix-ui";
 
 export type SelectGroupProps = React.ComponentProps<typeof SelectPrimitive.Group>;
 
