8e51b8031eb4fb213669a61037de5a1c6f3ae94a
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2026-01-07 12:43:47 +0100

    feat(tooltip): refactor Tooltip components and add TooltipTrigger and TooltipContent

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -1,46 +1,15 @@
 import * as React from "react";
 
-import { Tooltip as ReactTooltip } from "radix-ui";
+import { Tooltip as TooltipPrimitive } from "radix-ui";
 
-export type TooltipProps = ReactTooltip.TooltipContentProps & {
-  children?: React.ReactNode;
-  defaultOpen?: boolean;
-  open?: boolean;
-  onOpenChange?: (open: boolean) => void;
-  content?: React.ReactNode;
-  collisionPadding?: number;
-};
+import { TooltipProvider } from "./tooltip-provider";
 
-export function Tooltip({
-  defaultOpen,
-  content,
-  open,
-  onOpenChange,
-  children,
-  side,
-  align,
-  collisionPadding = 8,
-  sideOffset = 8,
-  ...props
-}: TooltipProps) {
+export type TooltipProps = React.ComponentProps<typeof TooltipPrimitive.Root>;
+
+export function Tooltip(props: TooltipProps) {
   return (
-    <ReactTooltip.Root delayDuration={0} open={open} defaultOpen={defaultOpen} onOpenChange={onOpenChange}>
-      <ReactTooltip.Trigger asChild>{children}</ReactTooltip.Trigger>
-      {content ? (
-        <ReactTooltip.Portal>
-          <ReactTooltip.Content
-            className="text-body-1 data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade rounded bg-white p-2 text-gray-100 will-change-[transform,opacity] select-none"
-            sideOffset={sideOffset}
-            side={side}
-            align={align}
-            collisionPadding={collisionPadding}
-            {...props}
-          >
-            {content}
-            <ReactTooltip.Arrow width={8} height={4} className="fill-white" />
-          </ReactTooltip.Content>
-        </ReactTooltip.Portal>
-      ) : null}
-    </ReactTooltip.Root>
+    <TooltipProvider>
+      <TooltipPrimitive.Root data-slot="tooltip" {...props} />
+    </TooltipProvider>
   );
 }
