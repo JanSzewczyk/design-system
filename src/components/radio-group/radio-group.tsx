5de8781b1c885ead2c6538744d165f6c1de6fe1e
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2026-02-21 22:25:24 +0100

    fix(radio-group): update RadioGroupProps type definition and add additional props

--- src/components/radio-group/radio-group.tsx
+++ src/components/radio-group/radio-group.tsx
@@ -4,7 +4,19 @@ import { RadioGroup as RadioGroupPrimitive } from "radix-ui";
 
 import { cn } from "~/utils";
 
-export type RadioGroupProps = RadioGroupPrimitive.RadioGroupProps;
+// TODO: fix after fixing the bug in radix-ui
+export type RadioGroupProps = React.ComponentPropsWithoutRef<"div"> & {
+  asChild?: boolean;
+  name?: string;
+  required?: boolean;
+  disabled?: boolean;
+  dir?: "ltr" | "rtl";
+  orientation?: "horizontal" | "vertical";
+  loop?: boolean;
+  defaultValue?: string;
+  value?: string | null;
+  onValueChange?: (value: string) => void;
+};
 
 export function RadioGroup({ className, ...props }: RadioGroupProps) {
   return <RadioGroupPrimitive.Root data-slot="radio-group" className={cn("grid gap-3", className)} {...props} />;
