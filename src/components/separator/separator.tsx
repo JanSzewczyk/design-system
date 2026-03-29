11b73a914c30ebfd892e39dc5da9b90778e23220
JanSzewczyk <jan.szewczyk1997@gmail.com>
2026-03-29 21:22:21 +0200

    feat: update Separator component to use new primitive and export Collapsible

--- src/components/separator/separator.tsx
+++ src/components/separator/separator.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 
-import { Separator as ReactSeparator } from "radix-ui";
+import { Separator as SeparatorPrimitive } from "radix-ui";
 
 import { cn } from "~/utils";
 
-export type SeparatorProps = React.ComponentProps<typeof ReactSeparator.Root>;
+export type SeparatorProps = React.ComponentProps<typeof SeparatorPrimitive.Root>;
 
 export function Separator({ className, orientation = "horizontal", decorative = false, ...props }: SeparatorProps) {
   return (
-    <ReactSeparator.Root
+    <SeparatorPrimitive.Root
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        "bg-border shrink-0 data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
         className
       )}
       {...props}
