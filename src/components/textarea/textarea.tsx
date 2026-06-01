9752a44fced641cc7a17c7784450e86554c1aff2
JanSzewczyk <jan.szewczyk1997@gmail.com>
2026-06-01 21:41:09 +0200

    fix(textarea): improve styling for better accessibility and user experience

--- src/components/textarea/textarea.tsx
+++ src/components/textarea/textarea.tsx
@@ -12,12 +12,8 @@ export function Textarea({ className, invalid = false, ...props }: TextareaProps
       data-slot="textarea"
       aria-invalid={props["aria-invalid"] || invalid}
       className={cn(
-        "border-input dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none md:text-sm",
-        "placeholder:text-muted-foreground",
-        "disabled:cursor-not-allowed disabled:opacity-50",
-        "aria-invalid:ring-error/20 dark:aria-invalid:ring-error/40 aria-invalid:border-error",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        className
+          "flex field-sizing-content min-h-16 w-full rounded border border-input bg-transparent px-2.5 py-2 text-base transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
+          className
       )}
       {...props}
     />
