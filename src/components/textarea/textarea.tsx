6855b2f92ff569c619dea8933cb6945802a49a5d
JanSzewczyk <jan.szewczyk1997@gmail.com>
2026-06-12 14:22:37 +0200

    fix(carousel): add new carousel stories and integrate autoplay plugin

--- src/components/textarea/textarea.tsx
+++ src/components/textarea/textarea.tsx
@@ -12,8 +12,8 @@ export function Textarea({ className, invalid = false, ...props }: TextareaProps
       data-slot="textarea"
       aria-invalid={props["aria-invalid"] || invalid}
       className={cn(
-          "flex field-sizing-content min-h-16 w-full rounded border border-input bg-transparent px-2.5 py-2 text-base transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-error aria-invalid:ring-3 aria-invalid:ring-error/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-error/50 dark:aria-invalid:ring-error/40",
-          className
+        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 disabled:bg-input/50 aria-invalid:border-error aria-invalid:ring-error/20 dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-error/50 dark:aria-invalid:ring-error/40 flex field-sizing-content min-h-16 w-full rounded border bg-transparent px-2.5 py-2 text-base transition-colors outline-none focus-visible:ring-3 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-3 md:text-sm",
+        className
       )}
       {...props}
     />
